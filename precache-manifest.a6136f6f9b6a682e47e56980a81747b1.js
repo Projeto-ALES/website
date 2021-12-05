462f91f75f37cceeef6a5fb9fc97f0e779626f84
Jonas Kerwald <jkerwald@ucs.brm>
2021-12-05 20:35:50 -0300

    Updates

---
+++ precache-manifest.a6136f6f9b6a682e47e56980a81747b1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6891c1c224e6bf44f0b6230ad214e905",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "20f3613f4d1039ab6eba",
+    "url": "/website/static/css/2.02385a5c.chunk.css"
+  },
+  {
+    "revision": "95a66e371edaf0591b6f",
+    "url": "/website/static/css/main.c6d6b60b.chunk.css"
+  },
+  {
+    "revision": "20f3613f4d1039ab6eba",
+    "url": "/website/static/js/2.dfefec47.chunk.js"
+  },
+  {
+    "revision": "2b4f22986c4989540aab30549147f204",
+    "url": "/website/static/js/2.dfefec47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142d7c24d88e9b809c41",
+    "url": "/website/static/js/3.395c6bcd.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/website/static/js/3.395c6bcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f293bd1965792f7527d",
+    "url": "/website/static/js/4.fdaf0fdd.chunk.js"
+  },
+  {
+    "revision": "c0cd2c5f40481cc6aeff754b29a7cf95",
+    "url": "/website/static/js/4.fdaf0fdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7d1f03e86feace6d61",
+    "url": "/website/static/js/5.ea2558ba.chunk.js"
+  },
+  {
+    "revision": "54a13f759b1799645c0f4c440498a75c",
+    "url": "/website/static/js/5.ea2558ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95a66e371edaf0591b6f",
+    "url": "/website/static/js/main.2d67aa0b.chunk.js"
+  },
+  {
+    "revision": "5f080c2b6e8aec9fc297",
+    "url": "/website/static/js/runtime-main.06ce3790.js"
+  },
+  {
+    "revision": "5bf5a0ae29bc3e41f788c1154ab9d9e7",
+    "url": "/website/static/media/ales-hackathon.5bf5a0ae.jpg"
+  },
+  {
+    "revision": "018050b1a566c62b2adb5acfa8763a71",
+    "url": "/website/static/media/alessauro.018050b1.svg"
+  },
+  {
+    "revision": "ed89135e12ef617959483bcd216718c3",
+    "url": "/website/static/media/arrow-down.ed89135e.svg"
+  },
+  {
+    "revision": "0e74e77439dbb470b8c39f199f824997",
+    "url": "/website/static/media/logo.0e74e774.svg"
+  }
+]);
